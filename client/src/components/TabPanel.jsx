4dd02e47c83d7b6740a8325728e7a01ef4ad9ac9
jtywa <65796752+jtywa@users.noreply.github.com>
2023-03-11 12:36:46 -0600

    created functional tab panel using MUI styling

---
+++ client/src/components/TabPanel.jsx
@@ -0,0 +1,24 @@
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import { useState } from 'react';
+
+function TabPanel({ handleClick }) {
+  const [value, setValue] = useState('all');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    handleClick(newValue);
+    console.log(newValue)
+  };
+
+  return (
+    <Tabs value={value} onChange={handleChange}>
+      <Tab sx={{color: '#e8eaf6'}} label="All" value={'all'}></Tab>
+      <Tab sx={{color: '#e8eaf6'}} label="Open" value={'open'}></Tab>
+      <Tab sx={{color: '#e8eaf6'}} label="Completed" value={'completed'}></Tab>
+      <Tab sx={{color: '#e8eaf6'}} label="Archived" value={'archived'}></Tab>
+    </Tabs>
+    )
+}
+
+export default TabPanel
